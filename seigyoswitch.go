f29fc1b94f95072d624a22c23f132cf984f29d94
KeitoYuasa <>
2023-08-01 14:43:37 +0900

    add switch

--- seigyoswitch.go
+++ seigyoswitch.go
@@ -14,7 +14,7 @@ func SwitchSeigyo() {
 	case 4:
 		fmt.Println("良")
 	case 5:
-		fmt.Println("優良")
+		fmt.Println("優良") // 出力
 	default:
 		fmt.Println("単位0")
 	}
@@ -22,11 +22,23 @@ func SwitchSeigyo() {
 	txt := "running"
 	switch txt {
 	case "running":
-		fmt.Println("実行中")
-		fallthrough // "running"の場合も、実行中　停止中　の2つを出力させる。
+		fmt.Println("実行中") // 出力
+		fallthrough        // "running"の場合も、実行中　停止中　の2つを出力させる。
 	case "stop":
-		fmt.Println("停止中")
+		fmt.Println("停止中") // 出力
 	default:
 		fmt.Println("その他")
+	}
+
+	// カンマ区切りでcaseの条件を複数指定できる
+	// result := 1　出力なし
+	result := 90
+	switch result {
+	case 10, 20:
+		fmt.Println("問題あり")
+	case 50, 60:
+		fmt.Println("通常通り")
+	case 70, 80, 90, 100:
+		fmt.Println("快適") // 出力
 	}
 }
