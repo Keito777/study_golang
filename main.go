a244e12e44d809040a89592c1216b19b9f8a6d0f
KeitoYuasa <>
2023-08-12 11:35:31 +0900

    add struct

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
 /*
 以下main関数では、GO言語の機能別の挙動確認関数を定義している
 それぞれ、コメントを外して出力結果を確認する。
@@ -29,5 +37,45 @@ func main() {
 	// Seigyo()
 
 	// 関数
-	Allfunc()
+	// Allfunc()
+
+	// 構造体
+	// インスタンス化①（fieldは全てゼロ値）
+	var akiyama Person
+	fmt.Println(akiyama) // { 0 0}
+
+	// インスタンス化②（field順に全て初期化しないとエラー）
+	Baba := Person{"Baba", 20, 173}
+	fmt.Println(Baba) // {Baba 20 173}
+
+	// インスタンス化③（fieldを指定して初期化）
+	yamamoto := Person{age: 22, name: "yamamoto", height: 176}
+	fmt.Println(yamamoto) // {yamamoto 22 176}
+
+	// 値の代入
+	akiyama.age = 21
+	akiyama.name = "akiyama"
+	akiyama.height = 175
+	fmt.Println(akiyama) // {akiyama 21 175}
+
+	// JSON→構造体のfieldに代入
+	// タグ：``（JSONタグに指定したキーをもとにfieldに代入）
+	type Book struct {
+		Title      string    `json:"title"`
+		Author     string    `json:"author"`
+		Publisher  string    `json:"publisher"`
+		ReleasedAt time.Time `json:"released_at"`
+	}
+
+	f, err := os.Open("book.json")
+	if err != nil {
+		log.Fatal("file open error : ", err)
+	}
+	d := json.NewDecoder(f)
+	var b Book
+	d.Decode(&b)
+	fmt.Println(b.Title)      // クリエイターズ・ファイル
+	fmt.Println(b.Author)     // 秋山 竜次
+	fmt.Println(b.Publisher)  // ワニブックス
+	fmt.Println(b.ReleasedAt) // 2019-10-25 00:00:00 +0000 UTC
 }
